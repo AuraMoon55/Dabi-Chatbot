420c5fb4633b928f5af22ab09109539e0ae52985
Anshul Garg <garganshul553@gmail.com>
2021-12-29 00:44:59 +0530

    Delete plugin.py

--- Chatbot/plugin.py
+++
@@ -1,9 +0,0 @@
-from Chatbot import dispatcher, updater, SUPPORT_CHAT, ALIVE_PIC 
-
-
-def chumt():
-    if SUPPORT_CHAT is not None and isinstance(SUPPORT_CHAT, str):
-        try:
-            dispatcher.bot.send_photo(f"@{SUPPORT_CHAT}", f"{ALIVE_PIC}",  caption="Im alive to chat master")
-    updater.start_polling(timeout=15, read_latency=4)
-    updater.idle()
