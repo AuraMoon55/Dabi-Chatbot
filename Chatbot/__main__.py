bce24bd3baa26e4ad235c76283e2072c61290876
Anshul Garg <garganshul553@gmail.com>
2022-04-24 03:44:09 +0530

    Update __main__.py

--- Chatbot/__main__.py
+++ Chatbot/__main__.py
@@ -1,39 +1,33 @@
 import requests as r
-
-from telegram.ext import (
-    ConversationHandler,
-    CallbackQueryHandler,
-)
 import os, sys
 import json
 import html
-from telegram.ext.dispatcher import run_async
-from telegram import InlineKeyboardMarkup, InlineKeyboardButton, ForceReply
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram import Client
-from Chatbot import dispatcher, updater, pbot, BOT_USERNAME, ALIVE_PIC, SUPPORT_CHAT
+from Chatbot import pbot, BOT_USERNAME, ALIVE_PIC, SUPPORT_CHAT
 import asyncio
 from pyrogram import filters
-from telegram import Message, Chat, Update, Bot, MessageEntity
-from telegram.error import BadRequest, RetryAfter, Unauthorized
-from telegram.ext import CommandHandler, run_async, CallbackContext, MessageHandler
-from telegram.utils.helpers import mention_html, mention_markdown, escape_markdown
-from time import sleep
-from telegram import ParseMode
+
 
 START_MSG = """
 Hey there {}, I am {} 
 I am a chatbot you can either add me to group for chatting. 
-or you can chat here.\n For more information click on About button.\n
-For updates join my updates channel and for bot support join my support chat \n
+or you can chat here.
+For more information click on About button.
+
+For updates join my updates channel and for bot support join my support chat
 The links are given below."""
 
 ABT_MSG = """
-Hey there {}, this bot was made by \n
-• [PSYCHOPOMP](https://t.me/Horni_Senpaii) \n \n
-This bot uses kuki api to process your text and give a perfect reply. \n
-• [Kuki Api](https://www.kuki.ai/research) \n
-• [Pyrogram](https://docs.pyrogram.org) \n  \n
-Repository Link \n
+Hey there {}, this bot was made by
+• [PSYCHOPOMP](https://t.me/Horni_Senpaii)
+
+This bot uses kuki api to process your text and give a perfect reply.
+• [Kuki Api](https://www.kuki.ai/research)
+
+• [Pyrogram](https://docs.pyrogram.org)
+
+Repository Link
 • Not added yet 
 """
 
@@ -50,47 +44,38 @@ Abt_btn = InlineKeyboardMarkup([[InlineKeyboardButton(text="Back", callback_data
 
 
 @pbot.on_callback_query(filters.regex("abt"))
-async def faqs(update, context):
-    update.effectivemessage.edit(text=ABT_MSG.format(update.effective_user.first_name), reply_markup= Abt_btn, parse_mode=ParseMode.MARKDOWN)
+async def faqs(_, query):
+    await query.message.edit(text=ABT_MSG.format(query.from_user.first_name), reply_markup= Abt_btn)
 
 @pbot.on_callback_query(filters.regex("abt_close"))
-async def abt_close(update, context):
-    update.effective_message.delete()
+async def abt_close(_, query):
+    await query.message.delete()
 
 @pbot.on_callback_query(filters.regex("abt_back"))
-async def abt_back(update, context):
-    query.message.edit(text=START_MSG.format(context.bot.id, update.effective_user.first_name), reply_markup= Home_btn, parse_mode=ParseMode.MARKDOWN)
+async def abt_back(bot, query):
+    await query.message.edit(text=START_MSG.format(bot.id, query.from_user.first_name), reply_markup= Home_btn)
 
 @pbot.on_message(filters.command(["start", f"start@{BOT_USERNAME}"]) & filters.private)
-async def start(update: Update, context: CallbackContext):
-            msg = update.effective_message
-            umer_name = update.effective_user.first_name
-            bot = context.bot
-            msg.reply_text(START_MESSAGE.format(umer_name), 
-                              reply_markup=Home_btn,
-                              parse_mode=ParseMode.MARKDOWN)
+async def start(bot, message):
+            msg = message
+            umer_name = msg.from_user.first_name
+            await msg.reply_text(START_MESSAGE.format(umer_name), 
+                              reply_markup=Home_btn)
 
 @pbot.on_message(filters.command(["start", f"start@{BOT_USERNAME}"]) & filters.group )
-async def grp_start(message: Message, context: CallbackContext):
-             bot = context.bot
-             msg.reply_text("Thanks for adding me to this group. Feel free to chat.")
+async def grp_start(bot, messages):
+             await msg.reply_text("Thanks for adding me to this group. Feel free to chat.")
 
-@pbot.on_message(filters.text & filters.incoming & filters.private & filters.group)
-def chatbot(update: Update, context: CallbackContext):
-    message = update.effective_message
-    chat_id = update.effective_chat.id
-    bot = context.bot
-    if message.text and not message.document:
-        Message = message.text
-        bot.send_chat_action(chat_id, action="typing")
-        Chab = r.get('https://kukiapi.up.railway.app/Kuki/chatbot?message='+Message)
-        Chet = json.loads(Chab.text)
-        chet = Chet['reply']
-        sleep(0.3)
-        message.reply_text(chet, timeout=60)
+@pbot.on_message(filters.text & filters.incoming)
+async def chatbot(bot, message):
+    chat_id = message.chat.id
+    await bot.send_chat_action(chat_id, action="typing")
+    Chab = r.get('https://kukiapi.up.railway.app/Kuki/chatbot?message='+Message)
+    Chet = json.loads(Chab.text)
+    chet = Chet['reply']
+    sleep(0.3)
+    await message.reply_text(chet, timeout=60)
 
 if __name__ == "__main__":
-      dispatcher.bot.send_photo(f"@{SUPPORT_CHAT}", f"{ALIVE_PIC}",  caption="Im alive to chat master")
-      updater.start_polling(timeout=15, read_latency=4)
-      updater.idle()
+      pbot.send_photo(f"@{SUPPORT_CHAT}", f"{ALIVE_PIC}",  caption="Im alive to chat master")
       idle()
